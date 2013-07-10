b17886ed60e81e715788f221f3d928e82c5482ae
kostyukdg <kostyukdg@gmail.com>
2013-07-11 00:46:21 +0400

    default zf2 application

---
+++ module/Application/src/Application/Entity/User.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Application\Entity;
+use Doctrine\ORM\Mapping as ORM;
+/** @ORM\Entity */
+class User {
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    protected $id;
+
+    /** @ORM\Column(type="string") */
+    protected $fullName;
+
+    /**
+     * Set name
+     * @param $fullName
+     * @return $this
+     */
+    public function setFullName($fullName)
+    {
+        $this->fullName = $fullName;
+
+        return $this;
+    }
+
+    /**
+     * Get id
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
